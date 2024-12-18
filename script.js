f0b7d7a4ee74a4da6a637ad3515c7f5df4630377
@Tanketom <tanketom@gmail.com>
2024-12-18 10:14:30 +0100

    Update script.js

--- script.js
+++ script.js
@@ -6,13 +6,13 @@ async function generateCard() {
     const name = Math.random() < 0.2 ? (Math.random() < 0.5 ? "%first_name" : "Human") : nameInput;
     const greeting = `Season's Greetings, ${name}!
 
-As the holiday season approaches, we want to extend our ${getRandomWord(words.adjectives)} wishes to you and your loved ones. May this Christmas bring you ${getRandomWord(words.adjectives)}, ${getRandomWord(words.adjectives)}, and an abundance of ${getRandomWord(words.adjectives)} love. It's a time to celebrate the ${getRandomWord(words.adjectives)} warmth of family and friends, to cherish the ${getRandomWord(words.adjectives)} moments we share, and to create ${getRandomWord(words.adjectives)} memories that will last a lifetime.
+As the holiday season approaches, we want to extend our <em>${getRandomWord(words.adjectives)}</em> wishes to you and your loved ones. May this Christmas bring you <em>${getRandomWord(words.adjectives)}</em>, <em>${getRandomWord(words.adjectives)}</em>, and an abundance of <em>${getRandomWord(words.adjectives)}</em> love. It's a time to celebrate the <em>${getRandomWord(words.adjectives)}</em> warmth of family and friends, to cherish the <em>${getRandomWord(words.adjectives)}</em> moments we share, and to create <em>${getRandomWord(words.adjectives)}</em> memories that will last a lifetime.
 
-May your home be filled with ${getRandomWord(words.adjectives)} laughter, your heart with ${getRandomWord(words.adjectives)} contentment, and your days with ${getRandomWord(words.adjectives)} happiness. Whether you're enjoying a ${getRandomWord(words.adjectives)} feast, exchanging ${getRandomWord(words.adjectives)} gifts, or simply relaxing, we hope your holiday season is as ${getRandomWord(words.adjectives)} as you are.
+May your home be filled with <em>${getRandomWord(words.adjectives)}</em> laughter, your heart with <em>${getRandomWord(words.adjectives)}</em> contentment, and your days with <em>${getRandomWord(words.adjectives)}</em> happiness. Whether you're enjoying a <em>${getRandomWord(words.adjectives)}</em> feast, exchanging <em>${getRandomWord(words.adjectives)}</em> gifts, or simply relaxing, we hope your holiday season is as <em>${getRandomWord(words.adjectives)}</em> as you are.
 
-Thank you for being a part of our community. We look forward to sharing many more ${getRandomWord(words.adjectives)} moments with you in the coming year.
+Thank you for being a part of our community. We look forward to sharing many more <em>${getRandomWord(words.adjectives)}</em> moments with you in the coming year.
 
-${getRandomWord(words.adjectives)} Christmas and ${getRandomWord(words.adjectives)} New Year from the CDN!`;
+<em>${getRandomWord(words.adjectives)}</em> Christmas and <em>${getRandomWord(words.adjectives)}</em> New Year from the CDN!`;
 
     document.getElementById('card').innerText = greeting;
 }
